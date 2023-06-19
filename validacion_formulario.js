da1bdad701d29d89f2e9bf710251d0c13556e3bd
UrielSorlak <134745998+UrielSorlak@users.noreply.github.com>
2023-06-19 17:30:58 -0300

    intento
    intento

--- validacion_formulario.js
+++ validacion_formulario.js
@@ -1,10 +1,9 @@
 function validateForm(event) {
-    event.preventDefault(); // Evita que el formulario se envíe por defecto
+    event.preventDefault(); 
 
     var form = document.getElementById('myForm');
     formulario.addEventListener("submit", function(event) {
-        // Lógica de validación del formulario
-        // event.preventDefault(); // Si la validación no es exitosa, evita que se envíe el formulario
+        
       });
     var nombre = document.getElementById('nombre').value.trim();
     var email = document.getElementById('email').value.trim();
@@ -28,22 +27,20 @@ function validateForm(event) {
         return false;
     }
 
-    // Si el formulario es válido, puedes realizar alguna acción adicional, como enviar el email utilizando un servicio externo.
+    
     sendEmail(nombre, email, opinion);
 }
 
 function validateEmail(email) {
-    // Expresión regular para validar el formato de un email
+    
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
 
 function sendEmail(nombre, email, opinion) {
-    // Aquí puedes implementar la lógica para enviar el email utilizando un servicio externo,
-    // como una solicitud HTTP a una API de envío de emails.
-    // Por ejemplo, utilizando fetch o axios.
+    
 
-    // Ejemplo ficticio usando fetch:
+    
     var payload = {
         nombre: nombre,
         email: email,
@@ -60,8 +57,7 @@ function sendEmail(nombre, email, opinion) {
     .then(function(response) {
         if (response.ok) {
             alert('¡Email enviado con éxito!');
-            // Aquí puedes realizar alguna acción adicional después de enviar el email
-            // como mostrar un mensaje de éxito, redirigir a otra página, etc.
+            
         } else {
             alert('Hubo un error al enviar el email. Por favor, inténtelo nuevamente.');
         }
