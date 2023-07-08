4afc11b6878f8757fb49c92693a7c33633f9734a
UrielSorlak <134745998+UrielSorlak@users.noreply.github.com>
2023-07-08 18:14:00 -0300

    js de comidas

---
+++ java_script/validacion_receta_desayuno.js
@@ -0,0 +1,17 @@
+document.getElementById("formulario_receta_desayuno").addEventListener("submit", function(event) {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var receta = document.getElementById("receta_usuario_desayuno").value;
+    if (nombre === "" || email === "" || receta === "") {
+      alert("Por favor, completa todos los campos.");
+      event.preventDefault();
+    } else if (!validateEmail(email)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      event.preventDefault();
+    }
+  });
+  
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
